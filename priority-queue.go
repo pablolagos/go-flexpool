7ec6204a306ed57282a4287a30c3a0d05d9d37ed
Pablo Lagos <pablolagos@infocapital.cl>
2024-06-25 10:15:09 +0100

    Add comments to PriorityQueue methods

--- priority-queue.go
+++ priority-queue.go
@@ -1,17 +1,27 @@
 package flexpool
 
+// PriorityQueue is a heap-based priority queue of tasks.
 type PriorityQueue []*Task
 
+// Len returns the number of tasks in the queue.
 func (pq PriorityQueue) Len() int { return len(pq) }
+
+// Less compares the priority of two tasks.
 func (pq PriorityQueue) Less(i, j int) bool {
 	return pq[i].Priority > pq[j].Priority
 }
+
+// Swap exchanges the positions of two tasks in the queue.
 func (pq PriorityQueue) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
 }
+
+// Push adds a task to the queue.
 func (pq *PriorityQueue) Push(x interface{}) {
 	*pq = append(*pq, x.(*Task))
 }
+
+// Pop removes and returns the highest priority task from the queue.
 func (pq *PriorityQueue) Pop() interface{} {
 	old := *pq
 	n := len(old)
